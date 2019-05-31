b98274812a5c6728fdadc1b2d4fb8cbaea2aee5a
livia_welschinger <livia-welschinger@t-online.de>
2019-05-31 16:33:34 +0200

    Registered the service as an Eureka Client

--- src/main/java/de/livia/mwstrechner/MwstRechnerApplication.java
+++ src/main/java/de/livia/mwstrechner/MwstRechnerApplication.java
@@ -1,27 +1,37 @@
 package de.livia.mwstrechner;
 
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Application class
  *
  */
+@EnableDiscoveryClient
 @SpringBootApplication
-@EnableEurekaClient // makes the application act as an Eureka Client
-@RestController
 public class MwstRechnerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MwstRechnerApplication.class, args);
 	}
 
-	@RequestMapping(value = "/")
-	public String home(){
-		return "Eureka Client Application MwSt";
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	// http://localhost:8282/service-instances/my-first-eureka-client
+	@RequestMapping("/service-instances/{applicationName}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(
+			@PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
 	}
 
 }
